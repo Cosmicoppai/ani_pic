6157a52f3bb005f75e91dcbabe5eca86ef28a05d
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-09-17 20:14:25 +0530

    ♥

--- anipic/anipic/settings.py
+++ anipic/anipic/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wp4u1un^jz^-b6_)m$8*cc7hkf&s*mzvpz%3u3^ba&35%cb!s#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-wp4u1un^jz^-b6_)m$8*cc7hkf&s*mzvpz%3u3^ba&35%cb!s#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',]
 
 
 # Application definition
