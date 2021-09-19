af85507bbacbedc3bdae94745c508dca624e59a9
Cosmicoppai <kanakchaudhari12@gmail.com>
2021-09-19 19:43:53 +0530

    Added the tests ♥

--- anipic/quote/tests.py
+++ anipic/quote/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Quote
 
-# Create your tests here.
+
+class QuoteModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Quote.objects.create(quote="Haha", tag="trash")
+
+    def test(self):
+        quote = Quote.objects.get(id=1)
+        self.assertURLEqual(quote.quote, 'Haha')
+        self.assertEqual(quote.tag, 'trash')
+
+    def test_api(self):
+        pass
--- anipic/wallpaper/tests.py
+++ anipic/wallpaper/tests.py
@@ -1,3 +1,47 @@
+import os.path
 from django.test import TestCase
+from .models import WallPaper
+from io import BytesIO
+from PIL import Image
+from django.core.files import File
 
-# Create your tests here.
+
+
+class WallpaperModelTest(TestCase):
+
+    @staticmethod
+    def get_image_file(name='honda_tohru', ext='jpeg', size=(50, 50), color=(256, 0, 0)):
+        file_obj = BytesIO()
+        image = Image.new("RGB", size=size, color=color)
+        image.save(file_obj, ext)
+        file_obj.seek(0)
+        return File(file_obj, name=name)
+
+    @classmethod
+    def setUpTestData(cls):
+        WallPaper.objects.create(title='honda-tohru', image=WallpaperModelTest.get_image_file(), tag='fruit-basket', is_nsfw=False)
+
+
+    def test_title_content(self):
+        obj = WallPaper.objects.get(id=1)
+        expected_obj_name = f'{obj.title}'
+        self.assertEqual(expected_obj_name, 'honda-tohru')
+
+    def test_tag(self):
+        obj = WallPaper.objects.get(id=1)
+        expected_tag = f'{obj.tag}'
+        self.assertEqual(expected_tag, 'fruit-basket')
+
+    def test_is_nsfw(self):
+        obj = WallPaper.objects.get(id=1)
+        expected = f'{obj.is_nsfw}'
+        self.assertEquals(expected, 'False')
+
+
+    def test_image(self):
+        obj = WallPaper.objects.get(id=1)
+        expected_image = f'{obj.image}'
+        _image = self.get_image_file().name
+        if os.path.isfile(f"wallpapers/{_image}"):
+            os.remove(f"wallpapers/{_image}")
+        self.assertEqual(expected_image, _image)
