e1bf19565efa1e23ea7eec57674e41053e984a6b
CosmicOppai <kanakchaudhari12@gmail.com>
2021-10-26 09:38:12 +0530

    added the initialization script

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,5 +4,6 @@ set -e
 
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 gunicorn -w 4 anipic.wsgi:application --bind 0.0.0.0:9000
